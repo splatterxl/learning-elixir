dc47bf7fe167800d4f932d272b6068fb650ac777
Splatterxl <splatterxl@outlook.ie>
2022-03-26 20:37:41 +0000

    start values thing

---
+++ modules/values.exs
@@ -0,0 +1,15 @@
+# you may have noticed that we use <function>/<arity> to identify functions.
+# well, this is because we can actually capture the function value using this.
+
+&rem/2; # => &:erlang.rem/2, a reference to the function 
+
+# it's treated as an anonymous function 
+func = &rem/2;
+
+func.(3, 2); # => 1
+
+# you can also use it as shorthand for creating a function 
+
+hello = &("Hello, #{&1}!")
+
+IO.puts(hello.("world")); # => Hello, world!
